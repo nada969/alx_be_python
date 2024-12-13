6f60b8a1e9bcb02f195881153d31f180f65b10d4
Nada Yahya <nadamousa969@gmail.com>
2024-12-14 00:03:27 +0200

    update

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1:int = 10
+number2:int = 5
+Addition  = number1 + number2
+Subtraction = number2 - number1
+Multiplication = number1 * number2
+
+print("Addition of", number1, "and ",number2,"is",Addition)
+print("Subtraction of", number2, "and ",number1,"is",Subtraction)
+print("Multiplication of", number1, "and ",number2,"is",Multiplication)
