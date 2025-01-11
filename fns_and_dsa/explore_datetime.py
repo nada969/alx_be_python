521db919e3c0315516f684994c6f7a4254500437
nada yahya <56319084+nada969@users.noreply.github.com>
2025-01-11 14:34:33 +0200

    Update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,4 +1,4 @@
-from datetime import datetime 
+from datetime import datetime , timedelta
 def display_current_datetime():
     current_date = datetime.datetime.now()
     print('Current date and time:',current_date.strftime("%Y-%m-%d %H:%M:%S"))
