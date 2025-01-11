adb5200c44be8cf58a4bcfde7383d179d4931508
nada yahya <56319084+nada969@users.noreply.github.com>
2025-01-11 02:47:28 +0200

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -18,7 +18,7 @@ def main():
 
             if choice == '1':
                 # Prompt for and add an item
-                new_item =input(['\"]Enter the item to add: ['\"])
+                new_item =int(input("Enter the item to add:"))
                 shopping_list.append(new_item)
 
             elif choice == '2':
