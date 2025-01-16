0e1632cdaa77c91f678ab19fd755bcf868f3970a
nada969 <nadamousa969@gmail.com>
2025-01-16 14:28:07 +0200

    add bank account file :s `

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,23 +9,83 @@ def convert_to_fahrenheit(celsius):
     return temp
 
 
+def get_temp_value():
+    while True:
+        try:
+            return float(input('Enter the temperature to convert: '))
+        except ValueError as e:
+            print(f'there is an error: {e}')
+
+def get_temp_unit():
+    while True:
+        try:
+            unit = str(input('Is this temperature in Celsius or Fahrenheit? (C/F): ')).upper()
+            if unit in ["C", "F"]:
+                return unit
+            print("Invalid Temperature unit. please enter (C or F).")
+        except ValueError as e:
+            print(f'there is an error: {e}')
+
+def main():
+    value = get_temp_value()
+    unit = get_temp_unit()
+
+    if unit == 'C':
+        fahrenheit = convert_to_fahrenheit(value)
+        print(f'{value:.1f}°C is {fahrenheit:.1f}°F')
+    else:  # temp_unit == 'F'
+        celsius = convert_to_celsius(value)
+        print(f'{value:.1f}°F is {celsius:.1f}°C')
 
 
-temp = float(input('Enter the temperature to convert: '))
-type_temp =str(input('Is this temperature in Celsius or Fahrenheit? (C/F): '))
+# main()
+# # temp = float(input('Enter the temperature to convert: '))
+# # type_temp =str(input('Is this temperature in Celsius or Fahrenheit? (C/F): '))
 
-# if temp is float:
-if type_temp in ['C','c']:
-    c_temp = temp
-    f_temp = convert_to_fahrenheit(c_temp)
-    print(f'{c_temp}°C is {f_temp}°F')
+# # if temp is float:
+# if type_temp in ['C','c']:
+#     c_temp = temp
+#     f_temp = convert_to_fahrenheit(c_temp)
+#     print(f'{c_temp}°C is {f_temp}°F')
 
-elif type_temp in ['F','f']:
-    f_temp = temp
-    c_temp = convert_to_celsius(f_temp)
-    print(f'{f_temp}°F is {c_temp}°C')
-else:
-    print("Invalid Temperature unit. please enter (C or F).")
+# elif type_temp in ['F','f']:
+#     f_temp = temp
+#     c_temp = convert_to_celsius(f_temp)
+#     print(f'{f_temp}°F is {c_temp}°C')
 # else:
-#     print("Invalid temperature. Please enter a numeric value.")
+#     print("Invalid Temperature unit. please enter (C or F).")
+# # else:
+# #     print("Invalid temperature. Please enter a numeric value.")
 
+
+def super_think():
+    class root:
+        def a(self):
+            print("there is Root")    
+    class A(root):
+        # def a(self):
+        #     print("there is a")
+        #     super().a()
+        pass
+    class B(A):
+        def a(self):
+            print("there is BB")
+            super().a()
+
+    b = B()
+    b.a()
+
+# super_think()
+a = 5
+class A:
+      a = 7
+      pass
+
+class B(A):
+      pass
+
+class C(B):
+      pass
+
+c = C()
+print(c.a)
---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+
+class BankAccount:
+    def __init__(self,account_balance=0):
+        self.account_balance = account_balance 
+
+    def deposit(self,amount):
+        self.account_balance += amount
+    
+    def withdraw(self,amount):
+        self.account_balance -= amount
+        if self.account_balance >= 0:
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        print(f'the Balance is: {self.account_balance}')
+        return 0
