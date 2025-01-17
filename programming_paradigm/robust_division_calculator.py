28554ba6d71c19b2ae675996830ce3b03932d62e
nada969 <nadamousa969@gmail.com>
2025-01-17 15:06:31 +0200

    add robust_division_calculator file

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,12 +1,11 @@
 def safe_divide(numerator, denominator):
     try:
-        result = float(numerator) / float(denominator)
-        print(result)
+        result = numerator / denominator
+        return result
 
     except ValueError as e:
-        print(f' there is an error: {e}')
+        return (f' there is an error: {e}')
     
     except ZeroDivisionError as e:
-        print(f' there is an error: {e}')
+        return (f' there is an error: {e}')
     
-safe_divide("l",4)
