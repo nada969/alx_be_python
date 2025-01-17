65ed9099834ff12fc4cdd1ee2ef8115f8d93f242
nada yahya <56319084+nada969@users.noreply.github.com>
2025-01-17 15:09:27 +0200

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,6 @@
 def safe_divide(numerator, denominator):
     try:
-        result = numerator / denominator
+        result = float(numerator)/float(denominator)
         return result
 
     except ValueError as e:
